00eb77d3cb0edcfd7e972487cb0a28e61eb0195a
Ditidey <ditidey2017@gmail.com>
2023-05-18 12:25:33 +0100

    get method added

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-
  
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kos6m2u.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -23,8 +19,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+   
     await client.connect();
+
+     const toysCollections = client.db('creative-creators-toys').collection('educational-toys');
+    
+    app.post('/addToys', async(req, res)=>{
+        const data = req.body;
+        console.log(data);
+        const result = await toysCollections.insertOne(data);
+        res.send(result)
+    })
+    app.get('/allToys', async(req, res)=>{
+        const result = await toysCollections.find().toArray();
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
